56a893a150a5e8ecac11e34205983ee02bf51914
YasSerHbh <hbhyasser@gmail.com>
2022-12-19 14:41:56 +0100

    Added Scroller + Onclick Menu

--- js/chikh.js
+++ js/chikh.js
@@ -1,4 +1,5 @@
 
+// Birthday
 var birthday = new Date('20 Dec 2022').getTime()
 
 var dayss = document.getElementById("days")
@@ -26,3 +27,45 @@ var counter = setInterval(() => {
     }
 
 }, 1000)
+
+
+
+
+
+// Scroller
+var scroller = document.querySelector(".scroller")
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+
+document.addEventListener("scroll", _ => {
+    var scrolltop = document.documentElement.scrollTop
+    scroller.style.width = `${(scrolltop / height) * 100}%`
+})
+
+
+
+
+
+// OnClick Menu
+var lili1 = document.querySelector(".lili1")
+var menu = document.querySelector(".menu")
+
+lili1.onclick = function() {
+    if (lili1.style.color === "var(--main-color)") {
+        lili1.style.color = "#000"
+        lili1.style.setProperty('--transf-before', 'rotateY(90deg)')
+        menu.style.display = "none"
+    }else {
+        lili1.style.color = "var(--main-color)"
+        lili1.style.setProperty('--transf-before', 'rotateY(0deg)')
+        menu.style.display = "block"
+    }
+}
+
+document.addEventListener("click", e => {
+    if (e.target.classList[0] !== "lili1") {
+    if (lili1.style.color === "var(--main-color)") {
+        lili1.style.color = "#000"
+        lili1.style.setProperty('--transf-before', 'rotateY(90deg)')
+        menu.style.display = "none"
+    }}
+})
